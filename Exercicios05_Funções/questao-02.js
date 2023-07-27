9e6b622d93cd334b4a53c5844be9cf63ac04bfd5
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-26 22:48:59 -0300

    Update questao-02.js

--- Exercicios05_Funções/questao-02.js
+++ Exercicios05_Funções/questao-02.js
@@ -6,8 +6,8 @@ function calcularCubo(numero) {
     }
     return Math.pow(numero, 3); //retorna o resultado da operação de potência, ou seja, a base(numero) elevada ao expoente(3)
 }
-var numeroInformado = parseFloat(prompt('Digite o numero:'));
-var media = calcularCubo(numeroInformado);
+let numeroInformado = parseFloat(prompt('Digite o numero:'));
+let media = calcularCubo(numeroInformado);
 if (media !== null) {
     alert('O cubo o número indicado é :' + media);
 } else {
