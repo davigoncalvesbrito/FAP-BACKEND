e7bc128150a00a255982fa1e046dfb3888976de0
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-27 22:16:18 -0300

    Update questao-08.js

--- Exercicios05_Funções/questao-08.js
+++ Exercicios05_Funções/questao-08.js
@@ -5,8 +5,11 @@ function retornoInvestimento(capitalInicial, tempoInvestimento, taxaJuros) {
     if (isNaN(capitalInicial) && isNaN(tempoInvestimento) && isNaN(taxaJuros)) {
         return null;
     }
-    const taxaDecimal = taxaJuros / 10;
-    const taxamensal = tempoInvestimento / 12;
+    const taxaDecimal = taxaJuros / 100; //// Convertendo a taxa de juros de percentual para decimal
+    const taxamensal = taxaJuros / 12;
+    /* Se o tempo de investimento for fornecido em meses, é necessário ajustar a taxa de juros para refletir a periodicidade do investimento.
+    Isso pode ser feito dividindo a taxa de juros anual pelo número de meses no ano (que é 12).*/
+   
     const montante = capitalInicial * Math.pow(1 + taxaDecimal, taxamensal);
     return montante;
 }
