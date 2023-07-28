68699a363aaeaa1a7c509608a5274e8461b1d669
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-27 22:02:40 -0300

    Update questao-07.js

--- Exercicios05_Funções/questao-07.js
+++ Exercicios05_Funções/questao-07.js
@@ -1,3 +1,6 @@
+/*Escreva uma função que permita contar o número de vogais contidas em uma string fornecida pelo usuário.
+Por exemplo, o usuário informa a string “Beterraba”, e a função retorna o número 4 (há 4 vogais nessa palavra).*/
+
 function contarVogais(letra) {
     const vogais = 'aeiouAEIOU';
     let contador = 0;
