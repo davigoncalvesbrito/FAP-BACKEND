3ecc6033c73c3a102d5e22b04382840ea94cc75c
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-27 21:03:52 -0300

    Update questao-06.js

--- Exercicios05_Funções/questao-06.js
+++ Exercicios05_Funções/questao-06.js
@@ -1,3 +1,5 @@
+//Escreva uma função que mostre na tela um número fornecido pelo usuário, porém invertido. Por exemplo, o usuário fornece o número 875 e a função mostra na tela o número 578.
+
 function inverterNumero(numero) {
     if (isNaN(numero)) {
         return null;
