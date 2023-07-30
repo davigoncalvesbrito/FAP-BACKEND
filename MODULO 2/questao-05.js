0b6c20c47d22d9b1a86d527bd5c8ddfa2cbfc13d
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-30 14:52:41 -0300

    Update questao-05.js

--- MODULO 2/questao-05.js
+++ MODULO 2/questao-05.js
@@ -1,6 +1,6 @@
 console.log('Iniciando Contagem Regressiva ');
 
-for (let i = 5; i >= 1; i--) {
+for (let i = 5; i >= 0; i--) {
     console.log('Contagem Regressiva ' + i + ' Segundos');
     if (i == 1) {
         console.log('BUUM!');
