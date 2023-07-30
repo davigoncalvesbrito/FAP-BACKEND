ed293f4a8c49ffe5dd388a73f37fa5aadac9b360
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-30 18:11:31 -0300

    Update questao-07.js

--- MODULO 2/questao-07.js
+++ MODULO 2/questao-07.js
@@ -10,31 +10,31 @@ function calculadora() {
             prompt('Digite o número da operação desejada : '),
         );
         if (operacao === 0) {
-            console.log('Saindo da Calculadora');
+            alert('Saindo da Calculadora');
             break;
-        } else if (operacao >= 1 && operacao <= 4) {
-            const n1 = parseFloat(prompt('Digite o primeiro número'));
-            const n2 = parseFloat(prompt('Digite o segundo número'));
-
-            let resultado;
-            if (operacao === 1) {
-                resultado = n1 + n2;
-            } else if (operacao === 2) {
-                resultado = n1 - n2;
-            } else if (operacao === 3) {
-                resultado = n1 * n2;
-            } else if (operacao === 4) {
-                if (n2 !== 0) {
-                    resultado = n1 / n2;
-                } else {
-                    alert('Divisão por zero não é permitida');
-                    continue;
-                }
-            }
-            alert('O Resultado é: ' + resultado);
-        } else {
-            alert('Essa opção não existe');
         }
+        let n1 = parseFloat(prompt('Digite o primeiro número'));
+        let n2 = parseFloat(prompt('Digite o segundo número'));
+        let resultado;
+
+        if (operacao === 1) {
+            resultado = n1 + n2;
+        } else if (operacao === 2) {
+            resultado = n1 - n2;
+        } else if (operacao === 3) {
+            resultado = n1 * n2;
+        } else if (operacao === 4) {
+            if (n2 != 0) {
+                resultado = n1 / n2;
+            } else {
+                alert('Divisão por zero não é permitida');
+                continue;
+            }
+        } else {
+            alert('essa opção não existe');
+            continue;
+        }
+        alert('O Resultado é: ' + resultado);
     }
 }
 calculadora();
