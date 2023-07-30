69263d60a788c9aa85e1c4d3356cc279985bc9fd
Davi Gonçalves <70551067+davigoncalvesbrito@users.noreply.github.com>
2023-07-30 14:49:52 -0300

    Add files via upload

---
+++ MODULO 2/questao-05.js
@@ -0,0 +1,8 @@
+console.log('Iniciando Contagem Regressiva ');
+
+for (let i = 5; i >= 1; i--) {
+    console.log('Contagem Regressiva ' + i + ' Segundos');
+    if (i == 1) {
+        console.log('BUUM!');
+    }
+}
